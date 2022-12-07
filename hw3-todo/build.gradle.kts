2a44e504fd3ef5c23b35d47c4ebd641bf442d057
geracimov <geracimov@gmail.com>
2022-12-07 23:17:41 +0300

    hw3-todo source code

--- hw3-todo/build.gradle.kts
+++ hw3-todo/build.gradle.kts
@@ -1,17 +1,18 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "1.7.10"
 }
 
-group = "org.example"
-version = "unspecified"
+group = "ru.geracimov.otus.kotlinqa.hw"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks.getByName<Test>("test") {
