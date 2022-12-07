cae97818288335070dfbabcbf3c855e400c0b77b
geracimov <geracimov@gmail.com>
2022-12-07 23:20:07 +0300

    hw3-todo tests

--- hw3-todo/build.gradle.kts
+++ hw3-todo/build.gradle.kts
@@ -11,7 +11,9 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+    testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testImplementation("io.mockk:mockk:1.13.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
